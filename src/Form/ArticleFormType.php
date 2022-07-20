830ff68aa4f4302bc851151accd2780172b7e613
KarimaAb <rima.bekhti@hotmail.com>
2022-07-20 15:58:42 +0200

    commit second

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +18,7 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType:: class, [
+            ->add('title', TextType::class, [
                 'label' => 'Titre'
             ])
             ->add('subtitle', TextType::class, [
@@ -27,7 +32,12 @@ class ArticleFormType extends AbstractType
                 'data_class' => null,
                 'attr' => [
                     'data-default-file' => $options['photo']
-                ]
+                ],
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie',
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Valider',
@@ -35,9 +45,7 @@ class ArticleFormType extends AbstractType
                 'attr' => [
                     'class' => 'd-block mx-auto my-3 col-3 btn btn-success'
                 ],
-
             ])
-
         ;
     }
 
@@ -47,7 +55,6 @@ class ArticleFormType extends AbstractType
             'data_class' => Article::class,
             'allow_file_upload' => true,
             'photo' => null
-
         ]);
     }
 }
